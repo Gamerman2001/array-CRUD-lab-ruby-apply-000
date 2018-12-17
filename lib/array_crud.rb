5ba2d760c0f6c35912d466046d3c029fd6c17a3f
Troy Gooden <tro12185@gmail.com>
2018-12-17 18:34:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [ "I",  "am", "really", "learning"]
-  element = "wow"
+  array = [ "wow", "I",  "am", "really", "learning"]
+  element = "arrays"
   array << element
 end
 
