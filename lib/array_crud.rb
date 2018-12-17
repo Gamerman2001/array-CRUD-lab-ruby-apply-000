5a6d8b454d5018279f1dc0fa09c80fb1f9c329ea
Troy Gooden <tro12185@gmail.com>
2018-12-17 18:46:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["we", "will", "remove", "something", "from", "the", "end"]
+  array = %w[we will remove something from the]
 end
 
 def remove_element_from_start_of_array(array)
