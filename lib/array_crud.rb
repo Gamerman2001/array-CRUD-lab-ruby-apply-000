6eb20fc0e0a17c4812e3e5e45bb7317b833b7194
Troy Gooden <tro12185@gmail.com>
2018-12-17 18:41:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,11 +17,10 @@ def add_element_to_start_of_array(array, element)
   array = ["you", "am", "really", "learning"]
   element = "wow"
   array.unshift(element)
-  
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["we", "will", "remove", "something", "from", "the", "end"]
 end
 
 def remove_element_from_start_of_array(array)
